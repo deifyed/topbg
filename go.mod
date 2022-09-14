4f4947f925b27f778ddcbc6fd50c64fd12a13df0
Julius Pedersen <deifyed@tutanota.com>
2022-09-14 17:58:05 +0200

    ✅ Add wm functionality

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/deifyed/topbg
 go 1.18
 
 require (
+	github.com/spf13/afero v1.9.2
+	github.com/spf13/cobra v1.5.0
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/spf13/cobra v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/text v0.3.4 // indirect
 )
---
+++ pkg/wm/api.go
@@ -0,0 +1,39 @@
+package wm
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os/exec"
+	"path"
+
+	"github.com/spf13/afero"
+)
+
+func SetBackground(fs *afero.Afero, imageType string, image io.Reader) error {
+        imagePath := path.Join(fs.GetTempDir(), fmt.Sprintf("current-topbg.%s", imageType))
+
+        err := fs.WriteReader(imagePath, image)
+        if err != nil {
+                return fmt.Errorf("writing image: %w", err)
+        }
+
+        swayset(imagePath)
+
+        return nil
+}
+
+func swayset(imagePath string) error {
+        cmd := exec.Command("swaymsg", "output", "*", "bg", imagePath, "stretch")
+
+        stderr := bytes.Buffer{}
+
+        cmd.Stderr = &stderr
+
+        err := cmd.Run()
+        if err != nil {
+                return fmt.Errorf("%s: %w", stderr.String(), err)
+        }
+
+        return nil
+}
