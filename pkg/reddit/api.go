ea933ce6f0b6dddd8bafdea84dde1765969e0ffe
Julius Pedersen <deifyed@tutanota.com>
2022-09-14 20:01:30 +0200

    ✅ Add subreddit image downloader functionality

---
+++ pkg/reddit/api.go
@@ -0,0 +1,115 @@
+package reddit
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func GetSubreddit(name string) ([]Image, error) {
+        posts, err := fetchTopPostsInSubreddit(name, 5)
+        if err != nil {
+                return nil, fmt.Errorf("fetching posts: %w", err)
+        }
+
+        urls := extractURLs(posts)
+
+        images := make([]Image, len(urls))
+
+        for index, url := range urls {
+                img, err := downloadImage(url)
+                if err != nil {
+                        return nil, fmt.Errorf("downloading %s: %w", url, err)
+                }
+
+                images[index] = img
+        }
+
+        return images, nil
+}
+
+func fetchTopPostsInSubreddit(name string, limit int) ([]topPostsResultDataChild, error) {
+        request, err := http.NewRequest(http.MethodGet, fmt.Sprintf(urlTemplate, name), nil)
+        if err != nil {
+                return nil, fmt.Errorf("preparing request: %w", err)
+        }
+
+        request.Header.Add("User-Agent", "topbg (by /u/deifyed)")
+
+        client := http.Client{}
+
+        response, err := client.Do(request)
+        if err != nil {
+                return nil, fmt.Errorf("executing request: %w", err)
+        }
+
+        rawBody, err := io.ReadAll(response.Body)
+        if err != nil {
+                return nil, fmt.Errorf("buffering body: %w", err)
+        }
+
+        var result topPostsResult
+
+        err = json.Unmarshal(rawBody, &result)
+        if err != nil {
+                return nil, fmt.Errorf("unmarshalling: %w", err)
+        }
+
+        relevantPosts := make([]topPostsResultDataChild, limit)
+        relevantIndex := 0
+
+        for i := 0; i < limit; i++ {
+                if valid(result.Data.Children[i]) {
+                        relevantPosts[relevantIndex] = result.Data.Children[i]
+
+                        relevantIndex++
+                }
+        }
+
+        return relevantPosts, nil
+}
+
+func downloadImage(url string) (Image, error) {
+        response, err := http.Get(url)
+        if err != nil {
+                return Image{}, fmt.Errorf("fetching: %w", err)
+        }
+
+        parts := strings.Split(url, ".")
+        extension := reverse(parts)[0]
+
+        return Image{
+                Type: extension,
+                Image: response.Body,
+        }, nil
+}
+
+func reverse(items []string) []string {
+        reversed := make([]string, len(items))
+        reversedIndex := len(items)-1
+
+        for _, item := range items {
+                reversed[reversedIndex] = item
+
+                reversedIndex--
+        }
+
+        return reversed
+}
+
+func extractURLs(items []topPostsResultDataChild) []string {
+        urls := make([]string, len(items))
+
+        for index, item := range items {
+                urls[index] = item.Data.URL
+        }
+
+        return urls
+}
+
+func valid(item topPostsResultDataChild) bool {
+        return !item.Data.Stickied
+}
+
---
+++ pkg/reddit/types.go
@@ -0,0 +1,27 @@
+package reddit
+
+import "io"
+
+type Image struct {
+        Type string
+        Image io.Reader
+}
+
+type topPostsResultDataChildData struct {
+        Stickied bool `json:"stickied"`
+        URL string `json:"url"`
+}
+
+type topPostsResultDataChild struct {
+        Data topPostsResultDataChildData `json:"data"`
+}
+
+type topPostsResultData struct {
+        Children []topPostsResultDataChild `json:"children"`
+}
+
+type topPostsResult struct {
+        Data topPostsResultData `json:"data"`
+}
+
+const urlTemplate = "https://www.reddit.com/r/%s.json?limit=3"
